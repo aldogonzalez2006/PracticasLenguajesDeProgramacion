fb22f6517a713e109ed02565a8c83482c650c38d
eyaelcr7 <eyaelcr7@ciencias.unam.mx>
2026-08-20 12:11:09 -0600

    Ya quedo la practica :)

--- Laboratorio01.hs
+++ Laboratorio01.hs
@@ -24,15 +24,38 @@ aplicaTresVeces :: (a -> a) -> a -> a
 aplicaTresVeces f x = f (f (f x))
 
 -- Reto 4
+--Bueno, pues aquí es formulaso, la única variable que tenemos que guardar es la media, la cual la guardamos en
+--'media', lo hicimos con let porque es más fácil para nosotros primero tener que guardar la variable que necesitamos
+-- y despues usarla en la formula grande que primero declarar la fórmula grande y despues las variables.
 varianza2 :: Double -> Double -> Double
+varianza2 x y = 
+  let media = (x+y)/2 in ((x-media)^2 + (y-media)^2)/2
 
 -- Reto 5
 clasificaTemperatura :: Int -> String
+--Este solo es un caso de if, en donde tenemos que poner restricciones de cuanto a cuanto se considera cada temperatura
+-- y este es perfecto para guardas :)
+clasificaTemperatura n 
+  | n <= 0        = "frio extremo"
+  | n <= 15       = "frio"
+  | n <= 24       = "templado"
+  | n <= 35       = "calido"
+  | otherwise     = "calor extremo"
 
 -- Reto 6
+--Aquí lo interesante de este ejercico son los casos base, en donde si la lista que nos dan desde el inicio es vacía, la regresamos tal como esta y ya
+--si hay elementos en donde se pueda poner el separador lo hacemos, tomamos la cabeza de la lista, añadimos el separador
+-- y retomamos el resto de la lista,  esa lista ya solo tahora si llegamos al punto dondeiene un elemento es nuestro freno de mano o caso base
+-- ya que nos dicen que no le agreguemos separador al final, regresamos esa lita tal y como esta y ya.
 intercala :: a -> [a] -> [a]
+intercala a []        = []  
+intercala a [x]       = [x]
+intercala a (x:xs)  = x : a : intercala a xs 
 
 -- Reto 7
+-- Esta función evalúa de forma recursiva una expresión algebraica con tipos propios, 
+-- separando los casos mediante pattern matching: si es un literal numérico solo extrae el valor, 
+-- y si es una suma o un producto, evalúa ambos lados de forma independiente y aplica la operación matemática
 data Expr
   = Lit Int
   | Suma Expr Expr
@@ -40,3 +63,6 @@ data Expr
   deriving (Eq, Show)
 
 evalua :: Expr -> Int
+evalua (Lit n)          = n
+evalua (Suma e1 e2)     = evalua e1 + evalua e2
+evalua (Producto e1 e2) = evalua e1 * evalua e2
